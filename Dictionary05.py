2d8006f4b0537bc12446130538caf3614b8bc485
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-01-28 10:32:01 +0530

    Create Dictionary05.py

---
+++ Dictionary05.py
@@ -0,0 +1,19 @@
+
+
+import pickle
+
+def save_dict(dict_to_save, file_path):
+    with open(file_path, 'wb') as file:
+        pickle.dump(dict_to_save, file)
+
+def load_dict(file_path):
+    with open(file_path, 'rb') as file:
+        return pickle.load(file)
+
+
+# commands used in solution video for reference
+if __name__ == '__main__':
+    test_dict = {1: 'a', 2: 'b', 3: 'c'}
+    save_dict(test_dict, 'test_dict.pickle')
+    recovered = load_dict('test_dict.pickle')
+    print(recovered)  # {1: 'a', 2: 'b', 3: 'c'}
