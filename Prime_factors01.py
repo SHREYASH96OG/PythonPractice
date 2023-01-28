284b5bbf1c5c811cdcb49c745916759ed7fe3b8d
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-01-28 10:14:16 +0530

    Create Prime_factors01.py

---
+++ Prime_factors01.py
@@ -0,0 +1,16 @@
+def get_prime_factors(number):
+    factors = []
+    divisor = 2
+    while divisor <= number:
+        if number % divisor == 0:
+            factors.append(divisor)
+            number = number // divisor
+        else:
+            divisor += 1
+    return factors
+
+
+# commands used in solution video for reference
+if __name__ == '__main__':
+    print(get_prime_factors(630))  # [2, 3, 3, 5, 7]
+    print(get_prime_factors(13))  # [13]
