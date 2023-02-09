93588b4a7f56d3bf3bc10832e3bed9323e231e56
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-09 23:41:42 +0530

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,16 @@
+# Python code to demonstrate Implementing 
+# stack using list
+stack = ["Amar", "Akbar", "Anthony"]
+stack.append("Ram")
+stack.append("Iqbal")
+print(stack)
+  
+# Removes the last item
+print(stack.pop())
+  
+print(stack)
+  
+# Removes the last item
+print(stack.pop())
+  
+print(stack)
