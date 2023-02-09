3ffcf7fe28a7f4edfacd26c6052b5c0980084bd2
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-09 23:42:31 +0530

    Create Queue.py

---
+++ Queue.py
@@ -0,0 +1,16 @@
+# Python code to demonstrate Implementing 
+# Queue using list
+queue = ["Amar", "Akbar", "Anthony"]
+queue.append("Ram")
+queue.append("Iqbal")
+print(queue)
+  
+# Removes the first item
+print(queue.pop(0))
+  
+print(queue)
+  
+# Removes the first item
+print(queue.pop(0))
+  
+print(queue)
