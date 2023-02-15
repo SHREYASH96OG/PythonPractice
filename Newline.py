c66e61595e8f7783158238e211d8fc4b51d31afd
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-15 21:05:03 +0530

    Create Newline.py

---
+++ Newline.py
@@ -0,0 +1,10 @@
+name = "Shreyash\n"
+# method1
+# m = "shreyash\n"*5
+# print(m)
+# method2
+# for i in range(6):
+#     print(name)
+# method3
+# In this method we should put after string \n
+# print(name*5)
