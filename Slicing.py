e360877f927317a7ef686fa69d2c3e75c1356a06
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-17 19:53:45 +0530

    Update Slicing.py

--- Slicing.py
+++ Slicing.py
@@ -1,7 +1,7 @@
 
 my_dict = [1,2,3,4,5,6,7]
 print(my_dict[0:8])
-#in this slicing method 0 representd  as initial element index and 8 is end index alternative way are [:8] or [0:]
+#in this slicing method 0 representd  as initial element index and 8 is end index alternative way are [:8] or [0:] or [:]
 print(my_dict[0:8:2])
 #here to 2 represented the index gap
 # O/P
