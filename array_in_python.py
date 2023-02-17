68a2cb1eecd3c57077eec4d5128d7855fa356b63
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-17 19:28:16 +0530

    Create array_in_python.py

---
+++ array_in_python.py
@@ -0,0 +1,9 @@
+#method:1
+import array
+numbers = array.array('i',[1,2,3,4,5,6]) #in quotes we mentioned datatype int
+print(numbers)
+
+# method:2
+import numpy as np
+nummber = np.array([1,2,3,4,5,6]) #THIS method don't need to mentioned the datatype
+print(number)
