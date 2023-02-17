b1d0ca5a5b3d1a53fb3899013bcc97ba8edcaac8
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-17 19:42:26 +0530

    Create dictionary_python.py

---
+++ dictionary_python.py
@@ -0,0 +1,12 @@
+my_dict = {"name":"shreyash","age":22,"Degree":"BE","l":[1,2,3,4]}
+x = my_dict.keys()
+print(x)
+#add new key value pair
+my_dict["college"]="SBPCOE"
+print(x)
+
+# O/P
+# dict_keys(['name', 'age', 'Degree', 'l'])
+# dict_keys(['name', 'age', 'Degree', 'l', 'college'])
+
+
