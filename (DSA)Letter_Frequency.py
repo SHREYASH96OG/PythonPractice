04e3409ce76023fb60dcaa161f124c0107c210a7
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-19 10:35:39 +0530

    Update (DSA)Letter_Frequency.py

--- (DSA)Letter_Frequency.py
+++ (DSA)Letter_Frequency.py
@@ -1,4 +1,15 @@
-#code is implemented to claculate percentage of substring from string
+# You are making a program to analyze text.
+# Take the text as the first input and a letter as the second input, and output the frequency of that letter in the text as a whole percentage.
+
+# Sample Input:
+# hello
+# l
+
+# Sample Output:
+# 40
+
+# Explanation : The letter l appears 2 times in the text hello, which has 5 letters. So, the frequency would be (2/5)*100 = 40.
+#Ans:
 n = input()
 l = input()
 c=n.count(l)
