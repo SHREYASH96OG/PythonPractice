83897532eca001c684134ebaeba4979f901f5588
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-19 10:48:30 +0530

    Update (DSA)Average_Word_Length.py

--- (DSA)Average_Word_Length.py
+++ (DSA)Average_Word_Length.py
@@ -1,3 +1,4 @@
+# Average Word Length
 # Given a sentence as input, calculate and output the average word length of that sentence.
 # To calculate the average word length, you need to divide the sum of all word lengths by the number of words in the sentence.
 
