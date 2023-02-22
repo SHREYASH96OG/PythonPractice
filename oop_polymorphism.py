f73b1366ea74045887925ba0bbd94fe4b671cc28
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-22 17:25:24 +0530

    Create oop_polymorphism.py

---
+++ oop_polymorphism.py
@@ -0,0 +1,22 @@
+class Polygon:
+    # method to render a shape
+    def render(self):
+        print("Rendering Polygon...")
+
+class Square(Polygon):
+    # renders Square
+    def render(self):
+        print("Rendering Square...")
+
+class Circle(Polygon):
+    # renders circle
+    def render(self):
+        print("Rendering Circle...")
+    
+# create an object of Square
+s1 = Square()
+s1.render()
+
+# create an object of Circle
+c1 = Circle()
+c1.render()
